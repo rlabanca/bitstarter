cbd7ea5b0cd1d6cb8dfe8ff3ef35eb4deb58819e
Rodrigo Labanca <rodrigo.labanca@gmail.com>
2013-07-10 01:53:33 +0000

    reading from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = fs.readFile('index.html');
+  var content = buf.toString();
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
